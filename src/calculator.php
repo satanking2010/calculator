ff112f2c908f087068a7ca4a087c0b9874249d6b
Your Name <you@example.com>
2017-05-29 15:50:03 +0700

    SecondCommit

---
+++ src/calculator.php
@@ -0,0 +1,20 @@
+<?php
+namespace satanknig2010\Calculator;
+class Calculator
+{
+    public function add($first_number , $second_number) {
+    return  $first_number + $second_number; }
+}
+{
+    public function subtract($first_number , $second_number) {
+    return  $first_number - $second_number; }
+}
+{
+    public function multiply($first_number , $second_number) {
+    return  $first_number * $second_number; }
+}
+{
+    public function divide($first_number , $second_number) {
+    return  $first_number / $second_number; }
+}
+?>
